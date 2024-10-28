bff387b65f27e9b780f58acccfa0e3f9ea6e6e66
MwangiGregory <gmwaburi@gmail.com>
2024-10-29 00:38:09 +0300

    Implement booting logic
    - Implement start up logic to start services and controller states
    - implement change to running state

--- src/controller/controller_booting.cpp
+++ src/controller/controller_booting.cpp
@@ -1,7 +1,21 @@
 #include "controller_states.h"
 
+// Definition of static variables inherited from ControllerState
+CAN_Service *ControllerState::can_system = nullptr;
+RTC_service *ControllerState::rtc_system = nullptr;
+
+void BootingState::create_services()
+{
+    ControllerState::can_system = CAN_Service::get_service_instance();
+    ControllerState::rtc_system = RTC_service::get_service_instance();
+}
+
 StatusCodes BootingState::start()
 {
+    started = true;
+    create_services();
+    create_controller_states();
+
     Error error = can_system->start_service();
     if (error.error_code != ErrCode::NONE)
         ErrorHandler::handle_error(error);
@@ -10,17 +24,30 @@ StatusCodes BootingState::start()
     if (error.error_code != ErrCode::NONE)
         ErrorHandler::handle_error(error);
 
+    if (error.error_code != ErrCode::NONE)
+        this->controller->set_controller_state(ErrorState::get_state_instance());
+
     Serial.println(rtc_system->get_timestamp());
     return StatusCodes::STATE_OK;
 }
 
 StatusCodes BootingState::run()
 {
+    if (started == false)
+    {
+        this->start();
+        started = true;
+    }
+
+    // transition to the RunningState
+    this->controller->set_controller_state(RunningState::get_state_instance());
     return StatusCodes();
 }
 
 StatusCodes BootingState::stop()
 {
+    if (started == false)
+        run();
     return StatusCodes();
 }
 
@@ -28,4 +55,11 @@ BootingState *BootingState::get_state_instance()
 {
     static BootingState booting_state;
     return &booting_state;
+}
+
+void BootingState::create_controller_states()
+{
+    RunningState::get_state_instance();
+    ErrorState::get_state_instance();
+    ShutdownState::get_state_instance();
 }
