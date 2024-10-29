9140470905e1391b2626f33c7be6e28624be58cd
MwangiGregory <gmwaburi@gmail.com>
2024-10-29 08:03:13 +0300

    Initialize gps service

--- src/controller/controller_booting.cpp
+++ src/controller/controller_booting.cpp
@@ -3,11 +3,13 @@
 // Definition of static variables inherited from ControllerState
 CAN_Service *ControllerState::can_system = nullptr;
 RTC_service *ControllerState::rtc_system = nullptr;
+GPS_Service *ControllerState::gps_system = nullptr;
 
 void BootingState::create_services()
 {
     ControllerState::can_system = CAN_Service::get_service_instance();
     ControllerState::rtc_system = RTC_service::get_service_instance();
+    ControllerState::gps_system = GPS_Service::get_service_instance();
 }
 
 StatusCodes BootingState::start()
@@ -16,11 +18,18 @@ StatusCodes BootingState::start()
     create_services();
     create_controller_states();
 
+    // maybe allocate an array to store service pointers for initialization, then destroy it when switching
+    // to another state
+
     Error error = can_system->start_service();
     if (error.error_code != ErrCode::NONE)
         ErrorHandler::handle_error(error);
 
     error = rtc_system->start_service();
+    if (error.error_code != ErrCode::NONE)
+        ErrorHandler::handle_error(error);
+
+    error = gps_system->start_service();
     if (error.error_code != ErrCode::NONE)
         ErrorHandler::handle_error(error);
 
